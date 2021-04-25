3250c07e8e28cc9abce8141606e4f98ae12957b1
Soumik Saha <iamsoumik18@gmail.com>
2021-04-25 10:00:47 +0530

    Added Search functionality

--- app/src/main/java/com/iam18/writeit/HomeFragment.kt
+++ app/src/main/java/com/iam18/writeit/HomeFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.iam18.writeit.adapter.NotesAdapter
 import com.iam18.writeit.database.NotesDatabase
 import com.iam18.writeit.entities.Notes
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class HomeFragment : BaseFragment() {
@@ -28,7 +32,6 @@ class HomeFragment : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
@@ -67,6 +70,55 @@ class HomeFragment : BaseFragment() {
         fabBtnCreateNote.setOnClickListener {
             replaceFragment(CreateNoteFragment.newInstance(),false)
         }
+
+        tick.setOnClickListener {
+            replaceFragment(CreateNoteFragment.newInstance(),false)
+        }
+
+        img.setOnClickListener {
+            replaceFragment(CreateNoteFragment.newInstance(),false)
+        }
+
+        link.setOnClickListener {
+            replaceFragment(CreateNoteFragment.newInstance(),false)
+        }
+
+        search_view.setOnQueryTextListener( object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                
+                var tempArr = ArrayList<Notes>()
+
+                for (arr in arrNotes){
+                    if (arr.title!!.toLowerCase(Locale.getDefault()).contains(p0.toString())){
+                        tempArr.add(arr)
+                    }
+                }
+                if(tempArr.size!=0) {
+                    notesAdapter.setData(tempArr)
+                    notesAdapter.notifyDataSetChanged()
+                }else{
+                    Toast.makeText(requireActivity(), "SORRY!!! NOTE NOT FOUND.", Toast.LENGTH_SHORT).show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+
+                var tempArr = ArrayList<Notes>()
+
+                for (arr in arrNotes){
+                    if (arr.title!!.toLowerCase(Locale.getDefault()).contains(p0.toString())){
+                        tempArr.add(arr)
+                    }
+                }
+
+                notesAdapter.setData(tempArr)
+                notesAdapter.notifyDataSetChanged()
+                return true
+            }
+
+        })
+
     }
 
     private val onClicked = object :NotesAdapter.OnItemClickListener{
